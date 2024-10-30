9d300f6868017ea6e7edfb0697b1976e158f7583
Cao Quan <quancao08@gmail.com>
2024-10-30 16:09:33 +0700

    add login register

--- app/src/main/java/com/example/prm_ecommerce/Activity/LoginActivity.java
+++ app/src/main/java/com/example/prm_ecommerce/Activity/LoginActivity.java
@@ -3,7 +3,9 @@ package com.example.prm_ecommerce.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,12 +14,16 @@ import com.example.prm_ecommerce.R;
 
 public class LoginActivity extends AppCompatActivity {
 
-
+    TextView txtAccount,txtPassword;
+    Button btnLogin;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        txtAccount = findViewById(R.id.edt_email_si);
+        txtPassword = findViewById(R.id.edt_pass_si);
+        btnLogin = findViewById(R.id.btn_login);
         TextView register = findViewById(R.id.createText);// Link to the layout file
         register.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -26,5 +32,24 @@ public class LoginActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+           if(Login(txtAccount.getText().toString(), txtPassword.getText().toString())){
+               Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+               startActivity(intent);
+
+               //thieu gan api va du lieu add bvao trong local
+           }
+           else{
+               Toast.makeText(LoginActivity.this, "Account or Password is wrong" , Toast.LENGTH_SHORT).show();
+           }
+            }
+        });
     }
+    private boolean Login(String txtEmaill , String txtPassword){
+        return false;
+    }
+
 }
